9e968187cfd2f1b633203a32ab409a81598a6d65
smgabss <122893759+smgabss@users.noreply.github.com>
2023-07-17 16:35:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const form = document.querySelector("#form-habits")
+button.addEventListener("click", photogen)
+
+
+function photogen() {
+
+  alert("Indo para o PhotoGen! Lembre de pedir para ele MOSTRAR uma imagem a você!")
+  
+}
+
+
+
